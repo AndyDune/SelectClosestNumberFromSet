2936a1447b11b7bdc121b33634392d240abb1626
Andrey Ryzhov <dune@rznlf.ru>
2018-04-12 20:29:36 +0300

    Add files.

---
+++ src/SelectClosestNumberFromRange.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * PHP version >= 5.6
+ *
+ * @package andydune/array-container
+ * @link  https://github.com/AndyDune/ArrayContainer for the canonical source repository
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author Andrey Ryzhov  <info@rznw.ru>
+ * @copyright 2018 Andrey Ryzhov
+ */
+
+
+namespace AndyDune;
+
+
+class SelectClosestNumberFromRange
+{
+
+}
